08382e8775af7158227ac5fefaa5c6c7761ce99e
pthomas2001 <71702126+pthomas2001@users.noreply.github.com>
2021-03-16 15:01:55 +0100

    js

--- app.js
+++ app.js
@@ -1,2 +1,29 @@
 var canvas = document.getElementById('myCanvas');
 var context = canvas.getContext('2d');
+
+var width = 113;
+var height = 98;
+
+function drawHexagon(positionX, positionY) {
+  context.beginPath();
+  context.moveTo(positionX, positionY);
+  context.lineTo(positionX + width * 0.25, positionY - height / 2);
+  context.lineTo(positionX + width * 0.75, positionY - height / 2);
+  context.lineTo(positionX + width, positionY);
+  context.lineTo(positionX + width * 0.75, positionY + height / 2);
+  context.lineTo(positionX + width * 0.25, positionY + height / 2);
+  context.lineTo(positionX, positionY);
+  context.strokeStyle = 'orange';
+  context.stroke();
+}
+
+drawHexagon(76, 120);
+drawHexagon(76, 230);
+drawHexagon(168.5, 65);
+drawHexagon(168.5, 175);
+drawHexagon(168.5, 285);
+drawHexagon(261, 120);
+drawHexagon(261, 230);
+
+context.fillStyle = 'orange';
+context.fill();
